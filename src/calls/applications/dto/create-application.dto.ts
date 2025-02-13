5b775725faf59e8ea6b8857fd11f03a5a1f7dc2d
Wilfried Musanzi <musanziw.m@gmail.com>
2025-02-13 12:16:09 +0200

    feat: update field type from JSON to string and integrate ReviewsModule

--- src/calls/applications/dto/create-application.dto.ts
+++ src/calls/applications/dto/create-application.dto.ts
@@ -2,5 +2,5 @@ import { IsNotEmpty } from 'class-validator';
 
 export class CreateApplicationDto {
   @IsNotEmpty()
-  responses: JSON;
+  responses: string;
 }
--- src/calls/applications/entities/application.entity.ts
+++ src/calls/applications/entities/application.entity.ts
@@ -1,14 +1,18 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 import { AbstractEntity } from '../../../shared/utils/abstract.entity';
 import { User } from '../../../users/entities/user.entity';
+import { Review } from '../reviews/entities/review.entity';
 
 @Entity()
 export class Application extends AbstractEntity {
   @Column({ type: 'json', nullable: true })
-  responses: JSON;
+  responses: string;
 
   @Column({ nullable: true })
   document: string;
+
+  @OneToMany(() => Review, (review) => review.application)
+  reviews: Review[];
 
   @ManyToOne(() => User, (applicant) => applicant.applications)
   @JoinColumn()
