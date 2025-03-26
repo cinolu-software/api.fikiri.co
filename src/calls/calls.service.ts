5b416655ea61fcea62f2dc5568d93aa815f01094
Wilfried Musanzi <musanziw.m@gmail.com>
2025-03-26 14:34:37 +0200

    feat: reintroduce reviewer management methods in CallsService

--- src/calls/calls.service.ts
+++ src/calls/calls.service.ts
@@ -3,7 +3,7 @@ import { CreateCallDto } from './dto/create-call.dto';
 import { UpdateCallDto } from './dto/update-call.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Call } from './entities/call.entity';
-import { LessThanOrEqual, MoreThan, Repository } from 'typeorm';
+import { IsNull, LessThanOrEqual, Not, Repository } from 'typeorm';
 import { User } from '../users/entities/user.entity';
 import * as fs from 'fs-extra';
 import { JwtService } from '@nestjs/jwt';
@@ -25,6 +25,119 @@ export class CallsService {
   async create(author: User, dto: CreateCallDto): Promise<Call> {
     try {
       return await this.callRepository.save({ ...dto, author });
+    } catch {
+      throw new BadRequestException();
+    }
+  }
+
+  async unpublish(id: string): Promise<Call> {
+    try {
+      const call = await this.findOne(id);
+      return await this.callRepository.save({
+        ...call,
+        published_at: null,
+        publisher: null
+      });
+    } catch {
+      throw new BadRequestException();
+    }
+  }
+
+  async publish(publisher: User, id: string, date: Date): Promise<Call> {
+    try {
+      const call = await this.findOne(id);
+      return await this.callRepository.save({
+        ...call,
+        publisher,
+        published_at: date ? new Date(date) : new Date()
+      });
+    } catch {
+      throw new BadRequestException();
+    }
+  }
+
+  async findLatest(): Promise<Call[]> {
+    return await this.callRepository.find({
+      where: { published_at: LessThanOrEqual(new Date()) },
+      relations: ['author'],
+      order: { published_at: 'DESC' },
+      take: 5
+    });
+  }
+
+  async findUnpublished(queryParams: QueryParams): Promise<[Call[], number]> {
+    const { page = 1 } = queryParams;
+    const take = 9;
+    const skip = (page - 1) * take;
+    return await this.callRepository.findAndCount({
+      where: { published_at: IsNull() },
+      order: { published_at: 'ASC' },
+      relations: ['author'],
+      take,
+      skip
+    });
+  }
+
+  async findPublished(queryParams: QueryParams): Promise<[Call[], number]> {
+    const { page = 1 } = queryParams;
+    const take = 9;
+    const skip = (page - 1) * take;
+    return await this.callRepository.findAndCount({
+      where: { published_at: Not(IsNull()) },
+      order: { published_at: 'ASC' },
+      relations: ['author'],
+      take,
+      skip
+    });
+  }
+
+  async findAll(): Promise<Call[]> {
+    return await this.callRepository.find({
+      order: { created_at: 'DESC' }
+    });
+  }
+
+  async addDocument(id: string, file: Express.Multer.File): Promise<Call> {
+    try {
+      const call = await this.findOne(id);
+      if (call.document) await fs.unlink(`./uploads/calls/documents/${call.document}`);
+      return await this.callRepository.save({
+        ...call,
+        document: file.filename
+      });
+    } catch {
+      throw new BadRequestException();
+    }
+  }
+
+  async addCover(id: string, file: Express.Multer.File): Promise<Call> {
+    try {
+      const call = await this.findOne(id);
+      if (call.cover) await fs.unlink(`./uploads/calls/covers/${call.cover}`);
+      return await this.callRepository.save({ ...call, cover: file.filename });
+    } catch {
+      throw new BadRequestException();
+    }
+  }
+
+  async findOne(id: string): Promise<Call> {
+    try {
+      return await this.callRepository.findOneOrFail({
+        where: { id },
+        relations: ['author']
+      });
+    } catch {
+      throw new NotFoundException();
+    }
+  }
+
+  async update(id: string, dto: UpdateCallDto): Promise<Call> {
+    try {
+      const call = await this.findOne(id);
+      return await this.callRepository.save({
+        ...call,
+        ...dto
+      });
     } catch {
       throw new BadRequestException();
     }
@@ -92,121 +205,6 @@ export class CallsService {
   async resendReviewLink(id: string, dto: IReviewer): Promise<void> {
     try {
       await this.sendReviewLink(id, dto);
-    } catch {
-      throw new BadRequestException();
-    }
-  }
-
-  async unpublish(id: string): Promise<Call> {
-    try {
-      const call = await this.findOne(id);
-      return await this.callRepository.save({
-        ...call,
-        published_at: null,
-        publisher: null
-      });
-    } catch {
-      throw new BadRequestException();
-    }
-  }
-
-  async publish(publisher: User, id: string, date: Date): Promise<Call> {
-    try {
-      const call = await this.findOne(id);
-      return await this.callRepository.save({
-        ...call,
-        publisher,
-        published_at: date ? new Date(date) : new Date()
-      });
-    } catch {
-      throw new BadRequestException();
-    }
-  }
-
-  async findLatest(): Promise<Call[]> {
-    return await this.callRepository.find({
-      where: { published_at: LessThanOrEqual(new Date()) },
-      relations: ['author'],
-      order: { published_at: 'DESC' },
-      take: 5
-    });
-  }
-
-  async findUnpublished(queryParams: QueryParams): Promise<[Call[], number]> {
-    const { page = 1 } = queryParams;
-    const take = 9;
-    const today = new Date();
-    const skip = (page - 1) * take;
-    return await this.callRepository.findAndCount({
-      where: { published_at: MoreThan(today) },
-      order: { published_at: 'ASC' },
-      relations: ['author'],
-      take,
-      skip
-    });
-  }
-
-  async findPublished(queryParams: QueryParams): Promise<[Call[], number]> {
-    const { page = 1 } = queryParams;
-    const take = 9;
-    const skip = (page - 1) * take;
-    const today = new Date();
-    return await this.callRepository.findAndCount({
-      where: { published_at: LessThanOrEqual(today) },
-      order: { published_at: 'ASC' },
-      relations: ['author'],
-      take,
-      skip
-    });
-  }
-
-  async findAll(): Promise<Call[]> {
-    return await this.callRepository.find({
-      order: { created_at: 'DESC' }
-    });
-  }
-
-  async addDocument(id: string, file: Express.Multer.File): Promise<Call> {
-    try {
-      const call = await this.findOne(id);
-      if (call.document) await fs.unlink(`./uploads/calls/documents/${call.document}`);
-      return await this.callRepository.save({
-        ...call,
-        document: file.filename
-      });
-    } catch {
-      throw new BadRequestException();
-    }
-  }
-
-  async addCover(id: string, file: Express.Multer.File): Promise<Call> {
-    try {
-      const call = await this.findOne(id);
-      if (call.cover) await fs.unlink(`./uploads/calls/covers/${call.cover}`);
-      return await this.callRepository.save({ ...call, cover: file.filename });
-    } catch {
-      throw new BadRequestException();
-    }
-  }
-
-  async findOne(id: string): Promise<Call> {
-    try {
-      return await this.callRepository.findOneOrFail({
-        where: { id },
-        relations: ['author']
-      });
-    } catch {
-      throw new NotFoundException();
-    }
-  }
-
-  async update(id: string, dto: UpdateCallDto): Promise<Call> {
-    try {
-      const call = await this.findOne(id);
-      return await this.callRepository.save({
-        ...call,
-        ...dto
-      });
     } catch {
       throw new BadRequestException();
     }
