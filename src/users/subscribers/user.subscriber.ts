3f9c577addb58ff02703ea438b1ec3e8bbecdf5c
Wilfried Musanzi <musanziw.m@gmail.com>
2025-05-14 09:40:16 +0200

    chore: include popularization link generation and streamline password hashing

--- src/users/subscribers/user.subscriber.ts
+++ src/users/subscribers/user.subscriber.ts
@@ -1,22 +1,23 @@
-import { EntitySubscriberInterface, EventSubscriber, InsertEvent, UpdateEvent } from 'typeorm';
+import { EntitySubscriberInterface, EventSubscriber, InsertEvent } from 'typeorm';
 import { User } from '../entities/user.entity';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @EventSubscriber()
 export class UserSubscriber implements EntitySubscriberInterface<User> {
+  constructor(private jwtService: JwtService) {}
+
   listenTo() {
     return User;
   }
 
   async beforeInsert(event: InsertEvent<User>): Promise<void> {
-    const { password } = event.entity;
-    if (!password) return;
-    event.entity.password = await bcrypt.hash(password, 10);
-  }
-
-  async beforeUpdate(event: UpdateEvent<User>): Promise<void> {
-    const { password } = event.entity;
-    if (!password) return;
-    event.entity.password = await bcrypt.hash(password, 10);
+    const user = event.entity;
+    if (!user.password) return;
+    user.password = await bcrypt.hash(user.password, 10);
+    user.popularization_link = await this.jwtService.signAsync(
+      { id: user.id, email: user.email },
+      { expiresIn: '30d' }
+    );
   }
 }
