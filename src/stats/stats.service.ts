b45f46d6ca6128b8e75634da490ddbd81f3fb87b
Wilfried Musanzi <musanziw.m@gmail.com>
2025-07-14 16:17:23 +0200

    refactor: update getAdminStats to count only non-deleted solutions and users

--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -7,14 +7,13 @@ export class StatsService {
   constructor(private dataSource: DataSource) {}
 
   async getAdminStats(): Promise<IAdminStats> {
-    const result = await this.dataSource.query(`
-    SELECT 
-      (SELECT COUNT(*) FROM call_solution) AS calls,
-      (SELECT COUNT(*) FROM solution) AS solutions,
-      (SELECT COUNT(*) FROM user WHERE deleted_at IS NOT NULL ) AS users,
-      (SELECT COUNT(*) FROM call_solution WHERE published_at IS NULL) AS unpublishedCalls,
-      (SELECT COUNT(*) FROM call_solution WHERE published_at IS NOT NULL) AS publishedCalls
-  `);
+    const result = await this.dataSource.query(`SELECT 
+        (SELECT COUNT(*) FROM call_solution) AS calls,
+        (SELECT COUNT(*) FROM solution WHERE deleted_at IS NULL) AS solutions,
+        (SELECT COUNT(*) FROM user WHERE deleted_at IS NULL) AS users,
+        (SELECT COUNT(*) FROM call_solution WHERE published_at IS NULL) AS unpublishedCalls,
+        (SELECT COUNT(*) FROM call_solution WHERE published_at IS NOT NULL) AS publishedCalls
+    `);
     return result[0];
   }
 }
